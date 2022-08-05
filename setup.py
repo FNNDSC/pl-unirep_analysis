7e6e57d9be7a372eea670661b5d7a22572c1a145
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2022-08-05 13:30:29 -0400

    Bump to 0.2.2 and rename module

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup
 
 setup(
     name             = 'unirep_analysis',
-    version          = '0.2.0',
+    version          = '0.2.2',
     description      = 'An app to run UniRep analysis',
     author           = 'Sandip Samal',
     author_email     = 'dev@babyMRI.org',
     url              = 'http://wiki',
-    py_modules       = ['unirep_tutorial'],
+    py_modules       = ['unirep_analysis'],
     test_suite       = 'nose.collector',
     tests_require    = ['nose'],
     packages         = ['src'],
@@ -18,7 +18,7 @@ setup(
     python_requires  = '>=3.5',
     entry_points     = {
         'console_scripts': [
-            'unirep_analysis = unirep_tutorial:main'
+            'unirep_analysis = unirep_analysis:main'
             ]
         }
 )
